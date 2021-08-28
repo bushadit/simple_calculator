34831d26fbe416eb792ef52d0509e9ef15da3045
Gabriel Gomes Borchardt <gabrielgborchardt@gmail.com>
2021-08-28 13:51:26 -0400

    operators buttons added and grid fixed

--- s_cal.py
+++ s_cal.py
@@ -5,7 +5,7 @@ root.title('Simple Calculator')
 
 # Entry (calculus)
 
-calc = Entry(root, width=35, borderwidth=5)
+calc = Entry(root, width=40, borderwidth=5)
 calc.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 # Defining buttons (numbers and operators)
@@ -26,20 +26,30 @@ button_8 = Button(root, text='8', padx=40, pady=20, command=button_add)
 button_9 = Button(root, text='9', padx=40, pady=20, command=button_add)
 button_0 = Button(root, text='0', padx=40, pady=20, command=button_add)
 
+button_clear = Button(root, text='Clear', padx=80, pady=20, command=button_add)
+button_addi = Button(root, text='+', padx=40, pady=20, command=button_add)
+button_minus = Button(root, text='-', padx=40, pady=20, command=button_add)
+button_equal = Button(root, text='=', padx=40, pady=20, command=button_add)
+
 # Putting the buttons on screen (grid)
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+button_1.grid(row=3, column=0, sticky=EW)
+button_2.grid(row=3, column=1, sticky=EW)
+button_3.grid(row=3, column=2, sticky=EW)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
+button_4.grid(row=2, column=0, sticky=EW)
+button_5.grid(row=2, column=1, sticky=EW)
+button_6.grid(row=2, column=2, sticky=EW)
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
+button_7.grid(row=1, column=0, sticky=EW)
+button_8.grid(row=1, column=1, sticky=EW)
+button_9.grid(row=1, column=2, sticky=EW)
 
-button_0.grid(row=4, column=0)
+button_0.grid(row=4, column=0, sticky=EW)
+
+button_clear.grid(row=4, column=1, columnspan=2, sticky=EW)
+button_addi.grid(row=5, column=0, sticky=EW)
+button_minus.grid(row=5, column=1, sticky=EW)
+button_equal.grid(row=5, column=2, sticky=EW)
 
 root.mainloop()
