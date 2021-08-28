0ba429385f3fa043fda0e94ae5b1400ae84b750a
Gabriel Gomes Borchardt <gabrielgborchardt@gmail.com>
2021-08-28 17:36:58 -0400

    button_add function renamed to button_click

--- s_cal.py
+++ s_cal.py
@@ -11,25 +11,26 @@ calc.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 # Defining buttons (numbers and operators)
 
 
-def button_add():
+def button_click():
     return
 
 
-button_1 = Button(root, text='1', padx=40, pady=20, command=button_add)
-button_2 = Button(root, text='2', padx=40, pady=20, command=button_add)
-button_3 = Button(root, text='3', padx=40, pady=20, command=button_add)
-button_4 = Button(root, text='4', padx=40, pady=20, command=button_add)
-button_5 = Button(root, text='5', padx=40, pady=20, command=button_add)
-button_6 = Button(root, text='6', padx=40, pady=20, command=button_add)
-button_7 = Button(root, text='7', padx=40, pady=20, command=button_add)
-button_8 = Button(root, text='8', padx=40, pady=20, command=button_add)
-button_9 = Button(root, text='9', padx=40, pady=20, command=button_add)
-button_0 = Button(root, text='0', padx=40, pady=20, command=button_add)
+button_1 = Button(root, text='1', padx=40, pady=20, command=button_click)
+button_2 = Button(root, text='2', padx=40, pady=20, command=button_click)
+button_3 = Button(root, text='3', padx=40, pady=20, command=button_click)
+button_4 = Button(root, text='4', padx=40, pady=20, command=button_click)
+button_5 = Button(root, text='5', padx=40, pady=20, command=button_click)
+button_6 = Button(root, text='6', padx=40, pady=20, command=button_click)
+button_7 = Button(root, text='7', padx=40, pady=20, command=button_click)
+button_8 = Button(root, text='8', padx=40, pady=20, command=button_click)
+button_9 = Button(root, text='9', padx=40, pady=20, command=button_click)
+button_0 = Button(root, text='0', padx=40, pady=20, command=button_click)
 
-button_clear = Button(root, text='Clear', padx=80, pady=20, command=button_add)
-button_addi = Button(root, text='+', padx=40, pady=20, command=button_add)
-button_minus = Button(root, text='-', padx=40, pady=20, command=button_add)
-button_equal = Button(root, text='=', padx=40, pady=20, command=button_add)
+button_clear = Button(root, text='Clear', padx=80,
+                      pady=20, command=button_click)
+button_addi = Button(root, text='+', padx=40, pady=20, command=button_click)
+button_minus = Button(root, text='-', padx=40, pady=20, command=button_click)
+button_equal = Button(root, text='=', padx=40, pady=20, command=button_click)
 
 # Putting the buttons on screen (grid)
 
